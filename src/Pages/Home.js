e582b962ebe112656c9735eecae9aa39960a46a7
Hustin <hustinkava@gmail.com>
2021-06-06 20:14:52 -0600

    navbar

---
+++ src/Pages/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../Styles/Home.css';
+import Navbar from '../Components/Navbar/Navbar';
+
+function Home() {
+    return (
+      <div>
+          <Navbar/>
+      </div>
+    );
+  }
+  
+  export default Home;
