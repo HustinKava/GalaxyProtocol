7e5d16acc324c96440bd6f9995b1bdbcc34659b7
Rachael McIlhagga <rmcilhagga@tvo.org>
2021-06-07 20:25:29 -0400

    jobs page and card development

---
+++ src/Components/Jobs/JobsHero.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Container } from '@material-ui/core';
+
+
+function JobsHero() {
+    return (
+        <div>
+            <h1>Work With Us</h1>
+
+            <p>If you get incredibly excited about Blockchain, cryptocurrency and everything that has to do with Starwars, you’re in the right place.</p>
+
+            <p>APPLY NOW</p>
+        </div>
+    )
+}
+
+export default JobsHero;
