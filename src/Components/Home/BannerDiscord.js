046b6e865560ee1a1090f0fd587b1ab1feddbe8c
Hustin <hustinkava@gmail.com>
2021-06-20 23:04:01 -0600

    20062021 update 2

--- src/Components/Home/BannerDiscord.js
+++ src/Components/Home/BannerDiscord.js
@@ -1,41 +1,42 @@
 import React from 'react';
-import { Grid, Container } from '@material-ui/core';
+import { Grid, Container, Box } from '@material-ui/core';
 import Discord from '../../Assets/Discord-art-1.png';
 
 function BannerDiscord() {
   return (
     <Grid container spacing={0} className="fadeRight" id="footerDiscord">
-      <Grid item xs={12} md={6}>
-        <img src={Discord} alt="Discord" className="discord" />
-      </Grid>
-      <Grid
-        item
-        xs={12}
-        md={6}
-        className="ban-gridPadding ban-contentContainer"
-      >
-
-<Container maxWidth="sm">
-
-
-        <div className="bannerTitleContainer">
-          <span className="banHeader-alt banHeader-alt--1">
-            STAY CONNECTED WITH US
-          </span>
-          <br />
-          <span className="banHeader-alt banHeader-alt--2">ON DISCORD</span>
-        </div>
-        <span className="banDesc banDesc--2">
-          Join our Discord channel to keep up with the latest updates and news
-          about everything we do and interact with one of our developers.
-        </span>
-        <div className="bannerButtonContainer">
-          <button className="ban-button ban-button--discord">Join now</button>
-        </div>
-
-</Container>
-
-      </Grid>
+      <Box clone order={{ xs: 2, sm: 2, md: 1 }}>
+        <Grid item xs={12} md={6}>
+          <img src={Discord} alt="Discord" className="discord" />
+          <div className="bannerButtonMobileContainer">
+            <Container maxWidth="sm">
+              <button className="ban-button ban-button--discord btn-mobile">
+                Join now
+              </button>
+            </Container>
+          </div>
+        </Grid>
+      </Box>
+      <Box clone order={{ xs: 1, sm: 1, md: 2 }}>
+        <Grid item xs={12} md={6} className="ban-gridPadding ban-contentContainer">
+          <Container maxWidth="sm" className='mobileCenter'>
+            <div className="bannerTitleContainer2">
+              <span className="banHeader-alt banHeader-alt--1">
+                STAY CONNECTED WITH US
+              </span>
+              <br />
+              <span className="banHeader-alt banHeader-alt--2">ON DISCORD</span>
+            </div>
+            <span className="banDesc banDesc--2">
+              Join our Discord channel to keep up with the latest updates and news
+              about everything we do and interact with one of our developers.
+            </span>
+            <div className="bannerButtonContainer">
+              <button className="ban-button ban-button--discord btn-desktop">Join now</button>
+            </div>
+          </Container>
+        </Grid>
+      </Box>
     </Grid>
   );
 }
--- src/Components/Home/BannerTelegram.js
+++ src/Components/Home/BannerTelegram.js
@@ -7,32 +7,34 @@ function BannerTelegram() {
     <div className="banner-container" id="footerTelegram">
       <Grid container spacing={0} className="fadeLeft">
         <Grid item xs={12} md={6} className="ban-gridPadding ">
-
-        <Container maxWidth="sm">
-
-
-          <div className="bannerTitleContainer">
-            <span className="banHeader-alt banHeader-alt--1">
-              STAY CONNECTED WITH US
+          <Container maxWidth="sm" className='mobileCenter'>
+            <div className="bannerTitleContainer2">
+              <span className="banHeader-alt banHeader-alt--1">
+                STAY CONNECTED WITH US
+              </span>
+              <br></br>
+              <span className="banHeader-alt banHeader-alt--2">ON TELEGRAM</span>
+            </div>
+            <span className="banDesc banDesc--2">
+              Join our Telegram channel to keep up with the latest updates and
+              interact with one of our developers.
             </span>
-            <br></br>
-            <span className="banHeader-alt banHeader-alt--2">ON TELEGRAM</span>
-          </div>
-          <span className="banDesc banDesc--2">
-            Join our Telegram channel to keep up with the latest updates and
-            interact with one of our developers.
-          </span>
-          <div className="bannerButtonContainer">
-            <button className="ban-button ban-button--telegram">
-              Join now
-            </button>
-          </div>
-
-</Container>
-
+            <div className="bannerButtonContainer">
+              <button className="ban-button ban-button--telegram btn-desktop">
+                Join now
+              </button>
+            </div>
+          </Container>
         </Grid>
         <Grid item xs={12} md={6}>
           <img className="telegram" src={Telegram} alt="Telegram" />
+          <div className="bannerButtonMobileContainer">
+            <Container maxWidth="sm">
+              <button className="ban-button ban-button--telegram btn-mobile">
+                Join Now
+              </button>
+            </Container>
+          </div>
         </Grid>
       </Grid>
     </div>
