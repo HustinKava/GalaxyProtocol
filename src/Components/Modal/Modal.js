c530668c56cb52e4b070f300e84343c64248b1bc
Hustin <hustinkava@gmail.com>
2021-07-01 22:29:05 -0600

    01-07-2021 update 3

--- src/Components/Modal/Modal.js
+++ src/Components/Modal/Modal.js
@@ -1,24 +1,27 @@
 import React from 'react';
+import { CgClose } from 'react-icons/cg';
 
 export const Modal = ({ modalContent, closeModal }) => {
-
   return (
-    <>  
-        <div className="modal">
-          <div
-            className="modal__content-container"
-            tabIndex={0}
-            onBlur={() => closeModal(false)}
-          >
-            <div className="modal__text">{modalContent}</div>
-            <button
+    <>
+      <div className="modal">
+        <div
+          className="modal__content-container"
+          tabIndex={0}
+          onBlur={() => closeModal(false)}
+        >
+          <div className="modal__content">
+            <CgClose
               onClick={() => closeModal(false)}
-              className="modal__button"
-            >
-              close
-            </button>
+              className="modal__close"
+            />
+            {modalContent}
           </div>
+          <button onClick={() => closeModal(false)} className="modal__button">
+            close
+          </button>
         </div>
+      </div>
     </>
   );
 };
