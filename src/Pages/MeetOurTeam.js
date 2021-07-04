401b002d8f1bb049973536b000f3eac963e92fec
Hustin <hustinkava@gmail.com>
2021-07-03 20:58:33 -0600

    hotfix meet page

--- src/Pages/MeetOurTeam.js
+++ src/Pages/MeetOurTeam.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import TeamHeader from '../Components/MeetOurTeam/TeamHeader';
 import OurTeam from '../Components/MeetOurTeam/OurTeam';
+import { Container } from '@material-ui/core';
 
 const MeetOurTeam = () => {
   return (
-    <div className="mainContainer meet-our-team">
+    <Container maxWidth='xl'>
+
+    <div className="mainContainer">
       <TeamHeader />
       <OurTeam />
     </div>
+
+    </Container>
   );
 };
 
